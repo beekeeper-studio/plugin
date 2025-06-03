a2a1636009338728319b48058ca964fd451c9819
azmy60 <mazmiagus2003@gmail.com>
2025-06-03 17:20:52 +0700

    nvm we dont need umd

--- dist/eventForwarder.d.ts
+++
@@ -1,3 +0,0 @@
-/** Any events that need to be forwarded to parent/plugin system
- * must go here */
-export {};
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,8 +4,6 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
 const input = "src/index.ts";
-/** FIXME: this file must be injected from the plugin system automatically */
-const inputEventForwarder = "src/eventForwarder.ts";
 
 export default [
   // ESM build
@@ -28,50 +26,11 @@ export default [
     external: [],
   },
   {
-    input: inputEventForwarder,
+    input: "src/eventForwarder.ts",
+    /** FIXME: this file must be injected from the plugin system automatically */
     output: {
       file: "dist/eventForwarder.js",
       format: "esm",
-      sourcemap: true,
-    },
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript({
-        tsconfig: "./tsconfig.json",
-        declaration: false,
-        outDir: "dist",
-      }),
-    ],
-    external: [],
-  },
-
-  // UMD build
-  {
-    input,
-    output: {
-      file: "dist/index.umd.js",
-      format: "umd",
-      name: "BeekeeperStudioPlugin",
-      sourcemap: true,
-    },
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript({
-        tsconfig: "./tsconfig.json",
-        declaration: false,
-        outDir: "dist",
-      }),
-    ],
-    external: [],
-  },
-  {
-    input: inputEventForwarder,
-    output: {
-      file: "dist/eventForwarder.umd.js",
-      format: "umd",
-      name: "BeekeeperStudioEventForwarder",
       sourcemap: true,
     },
     plugins: [
