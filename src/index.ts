c4adef3fe2e4960ffbdb79e2dc34379d4004c78f
Mohammad Azmi <mohammad.azmi.id@gmail.com>
2025-12-09 19:30:31 +0700

    move generateUUID

--- src/index.ts
+++ src/index.ts
@@ -317,23 +317,6 @@ export function notify(name: string, args: any) {
   window.parent.postMessage(payload, "*");
 }
 
-function generateUUID() {
-  const buf = new Uint8Array(16);
-  crypto.getRandomValues(buf);
-
-  buf[6] = (buf[6] & 0x0f) | 0x40; // version 4
-  buf[8] = (buf[8] & 0x3f) | 0x80; // variant
-
-  const hex = Array.from(buf, (b) => b.toString(16).padStart(2, "0")).join("");
-
-  return [
-    hex.substring(0, 8),
-    hex.substring(8, 12),
-    hex.substring(12, 16),
-    hex.substring(16, 20),
-    hex.substring(20),
-  ].join("-");
-}
 export function addNotificationListener(
   name: "tablesChanged",
   handler: () => void,
@@ -453,4 +436,22 @@ window.addEventListener("message", (event) => {
     }
   }
 });
+
+function generateUUID() {
+  const buf = new Uint8Array(16);
+  crypto.getRandomValues(buf);
+
+  buf[6] = (buf[6] & 0x0f) | 0x40; // version 4
+  buf[8] = (buf[8] & 0x3f) | 0x80; // variant
+
+  const hex = Array.from(buf, (b) => b.toString(16).padStart(2, "0")).join("");
+
+  return [
+    hex.substring(0, 8),
+    hex.substring(8, 12),
+    hex.substring(12, 16),
+    hex.substring(16, 20),
+    hex.substring(20),
+  ].join("-");
+}
 
